78db43a35a599415e914f3a1e8542642459cd595
ornaras <ornaras.us@gmail.com>
2026-02-09 08:02:45 +0400

    feat: Добавлен обработчик события BlockBreakEvent

--- src/main/java/ru/ornaras/tombstones/Events.java
+++ src/main/java/ru/ornaras/tombstones/Events.java
@@ -2,9 +2,13 @@ package ru.ornaras.tombstones;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.function.Predicate;
 
 public class Events implements Listener {
     private final JavaPlugin main;
@@ -24,5 +28,15 @@ public class Events implements Listener {
         block.setMetadata("owner", new FixedMetadataValue(main, tombstone.getOwnerId()));
         tombstone.save();
         drops.clear();
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e){
+        var b = e.getBlock();
+        if (b.getType() != Plugin.TombstoneMaterial) return;
+        Predicate<? super MetadataValue> filter = v -> v.getOwningPlugin() == main;
+        var owner = b.getMetadata("owner").stream().filter(filter);
+        if (owner.findAny().isEmpty()) return;
+        e.setCancelled(true);
     }
 }
