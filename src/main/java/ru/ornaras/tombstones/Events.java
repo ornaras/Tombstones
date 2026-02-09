573d9fd4df3289e7faa8015e52330bd310b1e9e4
ornaras <ornaras.us@gmail.com>
2026-02-09 08:04:57 +0400

    feat: Добавлен обработчик события PlayerInteractEvent

--- src/main/java/ru/ornaras/tombstones/Events.java
+++ src/main/java/ru/ornaras/tombstones/Events.java
@@ -1,9 +1,12 @@
 package ru.ornaras.tombstones;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -38,5 +41,25 @@ public class Events implements Listener {
         var owner = b.getMetadata("owner").stream().filter(filter);
         if (owner.findAny().isEmpty()) return;
         e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onTombstoneInteract(PlayerInteractEvent e){
+        if (e.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        var b = e.getClickedBlock();
+        if (b == null) return;
+        if (b.getType() != Plugin.TombstoneMaterial) return;
+        Predicate<? super MetadataValue> filter = v -> v.getOwningPlugin() == main;
+        var owner = b.getMetadata("owner").stream().filter(filter).toList();
+        if (owner.isEmpty()) return;
+        var player = e.getPlayer();
+        if (!player.getUniqueId().equals(owner.get(0).value())) return;
+        var tombstone = Tombstone.load(b.getLocation());
+        b.setType(Material.AIR);
+        b.removeMetadata("owner", main);
+        assert tombstone != null;
+        for (var drop : tombstone.getDrops())
+            player.getInventory().addItem(drop);
+        tombstone.remove();
     }
 }
