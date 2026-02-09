9e6a2a1e95472f4d3c417db9a0577431aadcf3b6
ornaras <ornaras.us@gmail.com>
2026-02-09 08:01:02 +0400

    feat: Добавлено восстановление метаданных блоков

--- src/main/java/ru/ornaras/tombstones/Plugin.java
+++ src/main/java/ru/ornaras/tombstones/Plugin.java
@@ -1,6 +1,7 @@
 package ru.ornaras.tombstones;
 
 import org.bukkit.Material;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Plugin extends JavaPlugin {
@@ -9,6 +10,17 @@ public final class Plugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new Events(this),this);
+        for(var world : getServer().getWorlds()){
+            for(var tombstone : Tombstone.loadAll(world)){
+                var block = tombstone.getLocation().getBlock();
+                if (block.getType() == TombstoneMaterial){
+                    var value = new FixedMetadataValue(this, tombstone.getOwnerId());
+                    block.setMetadata("owner", value);
+                }else{
+                    tombstone.remove();
+                }
+            }
+        }
     }
 
     @Override
