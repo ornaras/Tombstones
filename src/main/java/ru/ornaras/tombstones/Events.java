51fdafbe39fd816d672ccb4b94d5fbed8641282f
ornaras <ornaras.us@gmail.com>
2026-02-09 08:02:10 +0400

    feat: Добавлен обработчик события PlayerDeathEvent

--- src/main/java/ru/ornaras/tombstones/Events.java
+++ src/main/java/ru/ornaras/tombstones/Events.java
@@ -1,6 +1,9 @@
 package ru.ornaras.tombstones;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Events implements Listener {
@@ -8,5 +11,18 @@ public class Events implements Listener {
 
     public Events(JavaPlugin main){
         this.main = main;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e){
+        var player = e.getEntity();
+        var drops = e.getDrops();
+        if(drops.isEmpty()) return;
+        var tombstone = new Tombstone(player, drops);
+        var block = tombstone.getLocation().getBlock();
+        block.setType(Plugin.TombstoneMaterial);
+        block.setMetadata("owner", new FixedMetadataValue(main, tombstone.getOwnerId()));
+        tombstone.save();
+        drops.clear();
     }
 }
